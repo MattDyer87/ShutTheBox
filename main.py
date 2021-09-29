fae0a62c1c79309f08e3b67c12f4d52addd0b82e
MattDyer87 <71666354+MattDyer87@users.noreply.github.com>
2021-09-28 22:42:44 -0700

    more

--- main.py
+++ main.py
@@ -16,37 +16,73 @@ def roll():
     return roll
 
 def update_game_board(tab):
-    game_board[tab-1] = "*"
-    print("GameBoard:", "|".join(f"{t}" for t in game_board))
+    if tab != None:
+        game_board[tab-1] = "*"
+        
 
 while playing:
     first_die = roll()
     second_die = roll()
     print("Current Roll:", first_die, second_die)
 
-    choices = input("Enter the tabs you wish to flip separated by a space (q to quit)")
-    if choices.lower() == "q":
-        print("Thanks for playing!")
-        break
-    if len(choices) > 1:
-        choice1, choice2 = list(map(int, choices.split()))
-    else:
-        choice1 = choices
-        choice2 = choices
-    
-    if game_board[choice1-1] == "*" or game_board[choice2-1] == "*":
-        print("Your Score:", sum(remaining) )
-        break
-    
-    if game_board[choice1-1] + game_board[choice2-1] != first_die + second_die:
-        print("You must choose 2 that add up to your combined dice roll")
-        break
+    remaining = list(i for i in game_board if isinstance(i, int))
+    # for i, number in enumerate(remaining[:-1]):
+    #         complementary = first_die + second_die - number
+    #         if complementary in remaining[i+1:]:
+    #             choosing = True
+    #         else:
+    #             print("Bummer! Game Over!")
+    #             print("Your Score:", sum(remaining))
+    #             choosing = False
+    #             playing = False
+    #             break
+    choosing = True
 
+    while choosing:
+
+        choices = input("Enter the tabs you wish to flip separated by a space (q to quit)")
+        if choices[0].lower() == "q":
+            print("Thanks for playing!")
+            print("Your Score:", sum(remaining))
+            playing = False
+            choosing = False
+            break
+
+        if len(choices) > 3:
+            print("You must choose only 1 or 2 numbers")
+            continue
+
+        if len(choices) > 1:
+            choice1, choice2 = list(map(int, choices.split()))
+        else:
+            choice1 = choices[0]
+            choice2 = None
+        
+        if not isinstance(choice1, int) or not isinstance(choice2, int):
+            print("Your selections must be numbers from 1-9 or 'q' to quit")
+            continue
+
+        if choice1 + choice2 != first_die + second_die:
+            print("You must choose 2 that add up to your combined dice roll")
+            continue
+
+        if game_board[choice1-1] == "*" or game_board[choice2-1] == "*":
+            print("That has already been used")
+            continue
+
+        choosing = False
+            
     update_game_board(choice1)
     update_game_board(choice2)
+    print("GameBoard:", "|".join(f"{t}" for t in game_board))
+
+
+    
 
     remaining = list(i for i in game_board if isinstance(i, int))
+
     
+
     if remaining == 0:
         print("Congratulations! You Shut the Box!")
         break
